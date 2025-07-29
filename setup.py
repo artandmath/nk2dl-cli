5fd4a302c264a5feaffadae029065f0674a36301
Daniel H. <danielharkness@mac.com>
2025-07-29 10:33:57 +1000

    fix: Update setup.py for src/ directory structure

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/artandmath/nk2dl-cli",
-    packages=find_packages(where="python"),
-    package_dir={"": "python"},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "nk2dl>=0.1.0,<0.2.0",
     ],
